e080835ab2edc28c8a84933f964773319687a7f9
Balazs Gyurak <ba32107@gmail.com>
2020-11-17 20:40:47 +0000

    wip

--- assets/js/tabfloater.js
+++ assets/js/tabfloater.js
@@ -2,18 +2,6 @@ function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function showCopySuccessIndicators(visible) {
-    copyUbuntuPpaCodeIcon.hidden = visible;
-    copyUbuntuPpaCodeSuccessIcon.hidden = !visible;
-    copyUbuntuPpaCodeSuccessPopup.hidden = !visible;
-    if (visible) {
-        UIkit.drop(window.copyUbuntuPpaCodeSuccessPopup).show();
-    } else {
-        UIkit.drop(window.copyUbuntuPpaCodeSuccessPopup).hide();
-    }
-}
-
-
 function getOS() {
     const platformOrAppVersion = navigator.platform || navigator.appVersion;
     const osInfo = platformOrAppVersion.toString().toLowerCase();
@@ -31,7 +19,6 @@ function getOS() {
 
 function setPlatformSwitcher() {
     let platformId = 0;
-    console.log("switching");
 
     switch (getOS()) {
         case "Windows": platformId = 0; break;
@@ -42,11 +29,21 @@ function setPlatformSwitcher() {
     UIkit.switcher(window.platformSwitcher).show(platformId);
 }
 
-async function codeAreaClickedAsync() {
-    const textToCopy = window.ubuntuPpaCode.textContent.replaceAll("$ ", "") + "\n";
+async function codeContainerClickedAsync(codeBlock, copyIcon, copySuccessIcon, copySuccessPopup) {
+    function showCopySuccessIndicators(visible) {
+        copyIcon.hidden = visible;
+        copySuccessIcon.hidden = !visible;
+        if (visible) {
+            UIkit.drop(copySuccessPopup).show();
+        } else {
+            UIkit.drop(copySuccessPopup).hide();
+        }
+    }
+
+    const codeToCopy = codeBlock.textContent.replaceAll("$ ", "").trim() + "\n";
 
     try {
-        await navigator.clipboard.writeText(textToCopy);
+        await navigator.clipboard.writeText(codeToCopy);
         showCopySuccessIndicators(true);
         await delay(1500);
     } catch (error) {
@@ -56,13 +53,10 @@ async function codeAreaClickedAsync() {
     }
 }
 
-function qq() {
-    const codeBlocks = document.getElementsByTagName('pre');
-
-    debugger;
-    for (const block of codeBlocks) {
-        const parentDiv = block.parentElement;
-        parentDiv.classList.add("codeContainer", "uk-visible-toggle", "uk-position-relative");
+function addCopyElementsToPreBlocks() {
+    for (const preBlock of document.getElementsByTagName("pre")) {
+        const codeContainer = preBlock.parentElement;
+        codeContainer.classList.add("codeContainer", "uk-visible-toggle", "uk-position-relative");
 
         const iconsContainer = document.createElement("div");
         iconsContainer.classList.add("uk-position-center-right", "uk-flex-middle");
@@ -72,49 +66,42 @@ function qq() {
         copyIcon.setAttribute("uk-icon", "icon: copy");
 
         const copySuccessIcon = document.createElement("div");
-        copySuccessIcon.classList.add("uk-animation-fade", "uk-amination-fast", "uk-alert-success", "uk-border-pill");
+        copySuccessIcon.classList.add("uk-animation-fade", "uk-amination-fast", "uk-alert-success",
+                                      "uk-border-pill", "copyCodeSuccessIcon");
         copySuccessIcon.setAttribute("uk-icon", "icon: check");
         copySuccessIcon.setAttribute("hidden", "");
-
 
         iconsContainer.appendChild(copyIcon);
         iconsContainer.appendChild(copySuccessIcon);
 
+        const copySuccessPopupContainer = document.createElement("div");
+        copySuccessPopupContainer.classList.add("uk-width-auto");
+        copySuccessPopupContainer.setAttribute("uk-drop", "pos: bottom-center; toggle: #dropTarget");
 
-// <div uk-drop="pos: bottom-center; toggle: #dropTarget" id="copyUbuntuPpaCodeSuccessPopup" class="uk-width-auto">
-//                             <div class="uk-card-small uk-card-default uk-padding-small">Copied!</div>
-//                         </div>
-//                         <div hidden id="dropTarget" class="uk-position-center-right uk-padding-small"></div>
+        const copySuccessPopup = document.createElement("div");
+        copySuccessPopup.classList.add("uk-card-small", "uk-card-default", "uk-padding-small");
+        copySuccessPopup.textContent = "Copied!";
 
-
-        const successPopupContainer = document.createElement("div");
-        successPopupContainer.classList.add("uk-width-auto");
-        successPopupContainer.setAttribute("uk-drop", "pos: bottom-center; toggle: #dropTarget");
-
-        const successPopup = document.createElement("div");
-        successPopup.classList.add("uk-card-small", "uk-card-default", "uk-padding-small");
-        successPopup.textContent = "Copied!";
-
-        successPopupContainer.appendChild(successPopup);
+        copySuccessPopupContainer.appendChild(copySuccessPopup);
 
         const dropTarget = document.createElement("div");
+        dropTarget.id = "dropTarget";
         dropTarget.classList.add("uk-position-center-right", "uk-padding-small");
         dropTarget.setAttribute("hidden", "");
 
+        codeContainer.appendChild(iconsContainer);
+        codeContainer.appendChild(copySuccessPopupContainer);
+        codeContainer.appendChild(dropTarget);
 
-        parentDiv.appendChild(iconsContainer);
-        parentDiv.appendChild(successPopupContainer);
-        parentDiv.appendChild(dropTarget);
-        var q= 1;
-        q=2;
-
+        codeContainer.onclick = async function() {
+            const codeBlock = preBlock.children[0];
+            await codeContainerClickedAsync(codeBlock, copyIcon, copySuccessIcon, copySuccessPopupContainer);
+        }
     }
-
 }
 
-if (window.ubuntuPpaCodeContainer) {
-    window.ubuntuPpaCodeContainer.onclick = codeAreaClickedAsync;
+if (window.platformSwitcher) {
+    setPlatformSwitcher();
 }
 
-setPlatformSwitcher();
-qq();
+addCopyElementsToPreBlocks();
